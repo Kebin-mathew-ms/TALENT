bd6aea65b14075f7589217b40e7985c4f42cf00e
Kebin mathew <kebinmathew2003@gmail.com>
2026-09-23 01:05:16 +0530

    feat: enable local network device sharing

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.use(helmet());
 const allowedOrigin = process.env.CLIENT_URL || 'http://localhost:5173';
 app.use(
   cors({
-    origin: allowedOrigin,
+    origin: (origin, callback) => callback(null, true),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -57,7 +57,8 @@ app.use('/api', limiter);
 // Socket.io initialization
 const io = new Server(server, {
   cors: {
-    origin: allowedOrigin,
+    origin: (origin, callback) => callback(null, true),
+    credentials: true,
     methods: ['GET', 'POST'],
   },
 });
